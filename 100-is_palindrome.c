3bd2fee4686d470af15183fc3205bfdfa86dba7c
Ken Wekesa <wanyamak884@gmail.com>
2023-07-07 02:05:44 +0300

    betty rect

--- 100-is_palindrome.c
+++ 100-is_palindrome.c
@@ -25,6 +25,6 @@ int is_palindrome(char *s)
 	{
 		return (1);
 	}
-	
+
 	return (0);
 }
---
+++ 101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * wildcmp - compares two strings
+  * @s1: checks string 1
+  * @s2: checks string 2
+  *
+  * Return: 1 if string is identical, 0 otherwise
+  */
+int wildcmp(char *s1, char *s2)
+{
+
