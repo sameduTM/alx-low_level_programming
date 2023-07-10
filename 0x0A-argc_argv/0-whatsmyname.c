9a00dbd6848e2ed1910b68643448327c7b1a0a92
Ken Wekesa <wanyamak884@gmail.com>
2023-07-10 07:06:02 +0300

    betty error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -10,7 +9,7 @@
   */
 int main (int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main: checks the code
+  * @argc: checks the no. of arguments
+  * @argv: points to the array of strings
+  *
+  * Return: Always 0
+  */
+int main (int argc, char **argv)
+{
+
+}
