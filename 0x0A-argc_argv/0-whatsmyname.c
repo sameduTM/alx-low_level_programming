15b384ed1400091d14ed769716467cacdf996ebb
Ken Wekesa <wanyamak884@gmail.com>
2023-07-10 06:15:24 +0300

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - checks the code
+  * @argc: checks the argument count
+  * argv: points to the array of strings
+  *
+  * Return: Always 0
+  */
+int main(int argc, char **argv)
+{
+	printf("%s",argv[0]);
+	printf("\n");
+	(void)argc;
+
+	return (0);
+}
