4c0275c8c2d21ba0ab982832b7b415f663f2507f
Ken Wekesa <wanyamak884@gmail.com>
2023-07-10 07:18:57 +0300

    first commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-  * main: checks the code
+  * main - checks the code
   * @argc: checks the no. of arguments
   * @argv: points to the array of strings
   *
   * Return: Always 0
   */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
 
+	return (0);
 }
