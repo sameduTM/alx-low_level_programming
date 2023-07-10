a67e11cab46da8479bbd32f62028e97ab68fee9b
Ken Wekesa <wanyamak884@gmail.com>
2023-07-10 06:56:37 +0300

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - checks the code
+  * @argc: checks the number of args
+  * @argv: points to the array strings
+  *
+  * Return: Always void
+  */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
