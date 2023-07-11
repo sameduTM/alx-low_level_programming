e4b777ea91ce84fd7aae19a01585c1206ad419c3
Ken Wekesa <wanyamak884@gmail.com>
2023-07-11 16:18:17 +0300

    check malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,14 +5,14 @@
   * @size: checks size of pointer
   * @c: checks the array passed
   *
-  * Return: pointer to array, otherwise 0
+  * Return: pointer to array, otherwise NULL
   */
 char *create_array(unsigned int size, char c)
 {
-	char *str = malloc((size + 1) * sizeof(char));
+	char *str = malloc(size * sizeof(char));
 	unsigned int i = 0;
 
-	if (size == 0)
+	if (str == 0)
 	{
 		return (NULL);
 	}
