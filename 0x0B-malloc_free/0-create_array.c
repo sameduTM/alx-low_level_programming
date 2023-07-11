4814d50fbf3719dd4ae313f7b2d1c40bb9a88765
Ken Wekesa <wanyamak884@gmail.com>
2023-07-11 16:20:13 +0300

    size == 0 return Null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *str = malloc(size * sizeof(char));
 	unsigned int i = 0;
 
-	if (str == 0)
+	if (str == 0 || size == 0)
 	{
 		return (NULL);
 	}
