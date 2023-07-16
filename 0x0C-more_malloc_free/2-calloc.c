c617e7a187a574729eda3fa7d7c64fc41f8df1b5
Ken Wekesa <wanyamak884@gmail.com>
2023-07-17 01:07:55 +0300

    calloc fxn

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * _calloc - allocates memory for an array
+  * @nmemb: number of array elements
+  * @size: size of each data type
+  *
+  * Return: pointer to allocated memory, otherwise NULL
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * sizeof(char) * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	return (ptr);
+}
