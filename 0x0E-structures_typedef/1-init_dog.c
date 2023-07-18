d3ad06294416422b81149652f893760de296ebf0
Ken Wekesa <wanyamak884@gmail.com>
2023-07-18 04:14:11 +0300

    add descrption for Return value

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
   * init_dog - function that initialize struct dog
   * struct dog - initialize variable of type dog
@@ -11,7 +12,10 @@
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
 }
