d80f29574bbf3b53b489aba561e092bac0f43cb5
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 02:21:22 +0300

    rm trailing white space

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
-  * array_iterator - executes a function given as a 
+  * array_iterator - executes a function given as a
   * parameter on each element of an array.
   * @array: pointer to array variable
   * @size: size of the array
@@ -13,8 +13,10 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
+	unsigned int i = 0;
 
+	if (array == 0 || action == 0)
+		return;
 	while (i < size)
 	{
 		action(array[i]);
