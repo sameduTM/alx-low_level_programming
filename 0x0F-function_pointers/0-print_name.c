4539b5f950a9c84caf10ab9cfdd74e05a4fa6dbe
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 01:05:12 +0300

    first commit for function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+  * print_name - prints a name
+  * @name: pointer to name
+  * @f: pointer function
+  *
+  * Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
