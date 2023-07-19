24e6ce2addfd3fbe5f5fd133581c4f42df0e8716
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 02:16:03 +0300

    removed NULL check

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,10 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int i = 0;
 
-	if (array == NULL || action == NULL)
-	{
-		return;
-	}
 	while (i < size)
 	{
 		action(array[i]);
