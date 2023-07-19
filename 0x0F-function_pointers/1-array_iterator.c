1d71d241f71193e382dd4545317683107883c710
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 02:14:01 +0300

    function pointer to array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int i = 0;
 
-	if (array == NULL || size <= 0 || action == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
