5469c21748ddc74de16b7b04575f88e453a71908
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 20:29:53 +0300

    added return value for NULL instance

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,12 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	if (separator == NULL)
-	{
 		separator = "";
-		return;
-	}
-
-	str = "";
 
 	for (i = 0; i < n; i++)
 	{
