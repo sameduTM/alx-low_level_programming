4fd9cb81ddaea7798dab84140b2aa2d60a723ebc
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 20:05:47 +0300

    declared the string pointer to empty string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,6 +19,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		separator = "";
+
+	str = "";
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char*);
