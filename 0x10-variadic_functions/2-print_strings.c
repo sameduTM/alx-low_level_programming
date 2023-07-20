c1784bb7f985ae1cf556050f9b37726d0250bf80
Ken Wekesa <wanyamak884@gmail.com>
2023-07-20 20:12:32 +0300

    included curly braces

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,10 +27,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(ap, char*);
 
 		if (str == NULL)
+		{
 			printf("(nil)");
+		}
 		printf("%s", str);
 		if (i != n - 1)
+		{
 			printf("%s", separator);
+		}
 	}
 	va_end(ap);
 	printf("\n");
