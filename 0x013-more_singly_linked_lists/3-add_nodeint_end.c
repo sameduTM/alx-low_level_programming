cb845fceb1c9eb5f0884fe3538f07d5a9a867f03
Ken Wekesa <wanyamak884@gmail.com>
2023-08-01 01:45:09 +0300

    print list, set head to null

---
+++ 0x013-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+  * add_nodeint_end - adds new node at end
+  * @head: address of element
+  * @n: element on list
+  *
+  * Return: address of new element, otherwise NULL
+  */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *last = *head;
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
+
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new_node;
+
+	return (last);
+}
---
+++ 0x013-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a list
+  * @head: first element of list
+  *
+  * Return: nothing
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
