7d07a8f2a734756fd79a7a4c419450f6b3a795b9
Ken Wekesa <wanyamak884@gmail.com>
2023-08-02 02:26:22 +0300

    remove excess variables

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,30 +10,13 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current = malloc(sizeof(listint_t));
-
-	unsigned int i = 0;
-
-	if (current == NULL)
+	while (index--)
 	{
-		return (NULL);
+		if (head != NULL)
+			head = head->next;
+		else
+			return (NULL);
 	}
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	current = head;
-
-	while (head != NULL)
-	{
-		if (i == index)
-		{
-			break;
-		}
-		current = current->next;
-		i++;
-	}
-
-	return (current);
+	return (head);
 }
