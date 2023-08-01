2e675f6d8146c43aec45b36fc8215bc093cb6bf2
Ken Wekesa <wanyamak884@gmail.com>
2023-08-01 23:20:34 +0300

    description for function

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * delete_nodeint - deletes node at index
+  * delete_nodeint_at_index - deletes node at index
   * @head: pointer to head pointer
   * @index: position of node to be deleted
   *
