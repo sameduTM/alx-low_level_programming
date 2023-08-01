39a569b2da130b6200655240f7203e430068871e
Ken Wekesa <wanyamak884@gmail.com>
2023-08-01 23:25:01 +0300

    reverse a string

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+  * reverse_listint_t - reverses list
+  * @head: pointer to first element of list
+  *
+  * Return: pointer to first node of reversed list
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+
+	return (0);
+}
