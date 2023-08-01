4aaaa8b0e26f802d7ecdd5c41a3b04c4b6b43500
Ken Wekesa <wanyamak884@gmail.com>
2023-08-01 22:31:00 +0300

    insert at index

--- 0x013-more_singly_linked_lists/7-get_nodeint.c
+++ 0x013-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	unsigned int i = 0;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	current = head;
 
 	while (head != NULL)
