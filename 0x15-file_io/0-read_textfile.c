6efd7040b55339cbcdf08b5bb0798c74042e2ae7
Ken Wekesa <wanyamak884@gmail.com>
2023-09-04 16:53:23 +0300

    read text file and print to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * read_textfile - read textfile, prints
+  * @filename: file to read text from
+  * @letters: number of letters to print
+  *
+  * Return: number of letters
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char c;
+	size_t count = 0;
+
+	fp = fopen(filename, "r");
+
+	while (count < letters)
+	{
+		c = fgetc(fp);
+
+		if (feof(fp))
+			break;
+		putchar(c);
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef FILE_IO
+#define FILE_IO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+#endif
