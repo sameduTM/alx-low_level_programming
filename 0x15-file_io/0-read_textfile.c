394729053fb165c3fa5e720bc113f63bf53dd4ea
Ken Wekesa <wanyamak884@gmail.com>
2023-09-04 18:27:03 +0300

    changed the code to POSIX standard

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
   * read_textfile - read textfile, prints
@@ -10,22 +12,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	char c;
-	size_t count = 0;
+	int fp = 0, e = 0, ch_rd = 0;
+	char *buffer;
 
-	fp = fopen(filename, "r");
+	if (filename == NULL || letters == 0)
+		return (0);
 
-	while (count < letters)
+	fp = open(filename, O_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters + 1);
+
+	if (!buffer)
+		return (0);
+
+	ch_rd = read(fp, buffer, letters);
+
+	if (ch_rd == -1)
 	{
-		c = fgetc(fp);
-
-		if (feof(fp))
-			break;
-		_putchar(c);
-		count++;
+		free(buffer);
+		return (0);
 	}
-	fclose(fp);
 
-	return (count);
+	e = write(STDOUT_FILENO, buffer, ch_rd);
+
+	if (e <= 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fp);
+
+	return (ch_rd);
 }
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * create_file - creates a file
+  * @filename: file name
+  * @text_context: string to write to file
+  *
+  * Return: 1 on success, -1 on failure
+  */
+int create_file(const char *filename, char *text_content)
+{
+
