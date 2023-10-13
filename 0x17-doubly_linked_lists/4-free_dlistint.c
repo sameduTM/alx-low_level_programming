6b09f5ad17099a0509ac9b984460130f257b9d2e
Ken Wekesa <wanyamak884@gmail.com>
2023-10-13 03:46:02 +0300

    new node at endof list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Pointer to the head of the doubly-linked list.
+ *
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
