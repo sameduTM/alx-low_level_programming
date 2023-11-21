a9248fa07b3997dede070f3f92c5d4921385f893
Ken Wekesa <wanyamak884@gmail.com>
2023-11-21 13:40:38 +0300

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MORE_LOOPS
+#define MORE_LOOPS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+#endif
