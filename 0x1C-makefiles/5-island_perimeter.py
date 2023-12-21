f82f5753844489d85931884e63445804a5a9ff72
Ken Wekesa <wanyamak884@gmail.com>
2023-12-21 22:51:32 +0300

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island in the grid
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+
+    return perimeter
