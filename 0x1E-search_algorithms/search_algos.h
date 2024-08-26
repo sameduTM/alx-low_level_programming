0c9306bcab51156ca0602c9a19a77a58c3b0024b
Ken Wekesa <wanyamak884@gmail.com>
2024-08-26 10:03:44 +0300

    jump size algorithm search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,8 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+void check_size(int l, int h, int k, int *array);
+void check_value(int x, int *array, int value, int l, int h);
 
 #endif
