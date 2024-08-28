1ba155e01ba50fdc27fc2861d9580c8fa97b521f
Ken Wekesa <wanyamak884@gmail.com>
2024-08-28 16:53:32 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,24 +11,25 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int i = 0, m = sqrt(size), l = 0, h = m, k = size, x = 0, j;
+	int i = 0, m = sqrt(size), l = 0, h = m, sz = size;
 
-	j = size / m + (m - 1);
 	if (array == NULL)
 		return (-1);
 
-	while (i < j)
+	while (i < sz)
 	{
 		printf("Value checked array[%i] = [%i]\n", l, array[l]);
-		if (array[0] == value)
-			return (0);
-		if (array[h] > value)
+		if (h >= sz)
 		{
-			x = l + 1;
-			check_value(x, array, value, l, h);
-			return (x);
+			printf("Value found between indexes [%i] and [%i]\n", l, h);
+			while (l < sz)
+			{
+				printf("Value checked array[%i] = [%i]\n", l, array[l]);
+				l++;
+			}
+			return (-1);
 		}
-		if (array[h] == value)
+		if (array[h] >= value)
 		{
 			printf("Value found between indexes [%i] and [%i]\n", l, h);
 			while (l <= h)
@@ -38,57 +39,14 @@ int jump_search(int *array, size_t size, int value)
 					return (l);
 				l++;
 			}
-			return (h);
 		}
-		l = h;
-		h += m;
-		if (h > k)
+		if (array[h] < value)
 		{
-			check_size(l, h, k, array);
-			return (-1);
+			l = h;
+			h += m;
 		}
 		i++;
 	}
+
 	return (-1);
-}
-/**
- * check_size - check if high value is greater than array size
- * @l: low index of block
- * @h: high index of block
- * @k: array size
- * @array: array to check
- *
- * Return: Always -1
- */
-void check_size(int l, int h, int k, int *array)
-{
-	printf("Value checked array[%i] = [%i]\n", l, array[l]);
-	printf("Value found between indexes [%i] and [%i]\n", l, h);
-	while (l < k)
-	{
-		printf("Value checked array[%i] = [%i]\n", l, array[l]);
-		l++;
-	}
-}
-/**
- * check_value - checks if value is an element of array
- * @x: the index to be checked
- * @array: the array to be checked
- * @value: value to be checked
- * @l: lower index of block
- * @h: higher index of block
- *
- * Return: nothing
- */
-void check_value(int x, int *array, int value, int l, int h)
-{
-	if (array[x] == value)
-	{
-		printf("Value found between indexes [%i] and [%i]\n", l, h);
-		while (l < h - 1)
-		{
-			printf("Value checked array[%i] = [%i]\n", l, array[l]);
-			l++;
-		}
-	}
 }
--- 0x1E-search_algorithms/100-main.c
+++ 0x1E-search_algorithms/100-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "search_algos.h"
+
 /**
  * main - Entry point
  *
@@ -8,8 +9,8 @@
 int main(void)
 {
 	int array[15] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 9, 9, 11, 23, 47, 89, 160
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 23, 47, 89, 160
 	};
-	printf("Found at index: %d\n", jump_search(array, 15, 9));
+	printf("Found at index: %d\n", jump_search(array, 15, 11));
 	return (0);
 }
